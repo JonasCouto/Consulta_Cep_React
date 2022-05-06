16f5e8d12f99a7f6dfd98da579bfc3d5d9379480
JonasCouto <jonas-couto@hotmail.com>
2022-05-05 22:09:42 -0300

    :construction: Add new function in App.js
    Add new function in App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function App() {
-  
+function Search(){
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +13,53 @@ function App() {
       </header>
     </div>
   )
+}
+
+function Loading(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Carregando Resultados...</p>
+        <button> Cancelar </button>
+      </header>
+    </div>
+  )
+}
+
+function Result(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Resultado para o CEP 91520-110</p>
+        <span><b> Rua:</b> Teste </span>
+        <span><b> Cidade:</b> Porto Alegre </span>
+        <button> Nova Consulta </button>
+      </header>
+    </div>
+  )
+}
+
+function Errorr(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Erro na consulta</p>
+        <p> Servidor indisponiveis</p>
+        <button> Nova Consulta </button>
+      </header>
+    </div>
+  )
+}
+
+
+
+
+function App() {
+  
+  return Result()
   
 }
 
