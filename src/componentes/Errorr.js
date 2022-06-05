c29a9cbffb55c0483f9e22883eac8978e516660e
Jonas Couto <jonas-couto@hotmail.com>
2022-06-05 20:41:39 -0300

    :sparkles: add new function
    adding new function for using in error

--- src/componentes/Errorr.js
+++ src/componentes/Errorr.js
@@ -1,9 +1,10 @@
 function Errorr(props) {
   var navegar = props.navegar;
+  var erroMsg = props.erroMsg;
     return (
           <>  
-            <p>Erro na consulta</p>
-            <p>{props.errorMessage}</p>
+            <p>Erro na consulta </p>
+            <p>{erroMsg}</p>
             <button onClick={()=> navegar('Search')}> Nova Consulta </button>
           </>
     )
